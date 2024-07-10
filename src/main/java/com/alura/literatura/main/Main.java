19594e56a29deaffc97069dd0657963b32770daa
William Christopher Rendon Castruita <williamcastruita@proton.me>
2024-07-09 20:54:04 -0600

    Creando apiClient.java

--- src/main/java/com/alura/literatura/main/Main.java
+++ src/main/java/com/alura/literatura/main/Main.java
@@ -1,9 +1,17 @@
 package com.alura.literatura.main;
 
+import java.util.Scanner;
+
+import com.alura.literatura.service.ApiClient;
+
 public class Main {
+    private Scanner scanner = new Scanner(System.in);
+    private ApiClient apiClient = new ApiClient();
+    private final String URL_BASE = "https://gutendex.com/books/";
 
     public void showMenu() {
-        System.out.println("Hello world!");
+        String test = apiClient.getData(URL_BASE);
+        System.out.println(test);
     }
 
 }
